fc0e3cfbda5d984655ae9e9d41df1dc300996584
mengzhuoyou <mengzhuy@usc.edu>
2018-07-27 22:50:42 +0800

    build the initial page and the routes

---
+++ week1/oj-client/src/app/mock-problem.ts
@@ -0,0 +1,28 @@
+import { Problem } from "./models/problem.model";
+
+export const PROBLEMS: Problem[] = [
+  {
+    id: 1,
+    name: "Two Sum",
+    desc: "Given an array",
+    difficulty: "easy"
+  },
+  {
+    id: 2,
+    name: " 3Sum",
+    desc: "Given an array",
+    difficulty: "medium"
+  },
+  {
+    id: 3,
+    name: "4Sum",
+    desc: "Given an array",
+    difficulty: "hard"
+  },
+  {
+    id: 4,
+    name: "Minimum Sliding Window",
+    desc: "Sliding Window",
+    difficulty: "super"
+  }
+];
